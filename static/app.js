946cd14aa2ef6dc51a42f0dfc414fd377afa8d3c
Jayadeep KM <kmjayadeep@gmail.com>
2016-04-02 21:47:08 +0530

    cloud support started

--- static/app.js
+++ static/app.js
@@ -1,5 +1,45 @@
 var window = this;
 
+function parseString(str) {
+	// Create an object with no prototype
+	// https://github.com/sindresorhus/query-string/issues/47
+	var ret = Object.create(null);
+
+	if (typeof str !== 'string') {
+		return ret;
+	}
+
+	str = str.trim().replace(/^(\?|#|&)/, '');
+
+	if (!str) {
+		return ret;
+	}
+
+	str.split('&').forEach(function (param) {
+		var parts = param.replace(/\+/g, ' ').split('=');
+		// Firefox (pre 40) decodes `%3D` to `=`
+		// https://github.com/sindresorhus/query-string/pull/37
+		var key = parts.shift();
+		var val = parts.length > 0 ? parts.join('=') : undefined;
+
+		key = decodeURIComponent(key);
+
+		// missing `=` should be `null`:
+		// http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+		val = val === undefined ? null : decodeURIComponent(val);
+
+		if (ret[key] === undefined) {
+			ret[key] = val;
+		} else if (Array.isArray(ret[key])) {
+			ret[key].push(val);
+		} else {
+			ret[key] = [ret[key], val];
+		}
+	});
+
+	return ret;
+};
+
 //special angular merge
 var merge = function(x, y) {
 	if (!x || typeof x !== "object" ||
@@ -156,7 +196,7 @@ app.controller("OmniController", function($scope, $rootScope, storage) {
 
 	var parseMagnet = function(params) {
 		$scope.magnet = true;
-		var m = window.queryString.parse(params);
+		var m = parseString(params);
 
 		if (!/^urn:btih:([A-Za-z0-9]+)$/.test(m.xt)) {
 			$scope.omnierr = "Invalid Info Hash";
